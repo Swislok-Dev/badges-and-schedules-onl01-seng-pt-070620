388a6beffe3d4f367069a24f45f804c509c8865b
Cory Marker <cmarker4@gmail.com>
2020-07-18 04:10:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,7 +21,7 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
+  # puts batch_badge_creator(attendees)
   assign_rooms(attendees).each do |item|
     puts item
   end
