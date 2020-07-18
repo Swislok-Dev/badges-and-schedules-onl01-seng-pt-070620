bdffeb7bb9114e7b153e6f5c26721e316c7d0cbe
Cory Marker <cmarker4@gmail.com>
2020-07-18 04:08:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,7 +21,7 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
+  batch_badge_creator(attendees)
   i = 0
   while i < assign_rooms(attendees).size
     puts assign_rooms(attendees).index(i)
