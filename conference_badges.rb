fb2f22dacbca33b96f238b5243eb3267aa11a0fc
Cory Marker <cmarker4@gmail.com>
2020-07-18 03:43:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,8 +3,5 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.each do |attendees|
-    badges = []
-    badges.push(badge_maker(attendees))
-  end
+  badge_maker(attendees)
 end
