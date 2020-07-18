cbfe6c6971edc1e2ced67b6442cb2397f2d2d8ba
Cory Marker <cmarker4@gmail.com>
2020-07-18 03:55:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 def assign_rooms(attendees)
   room_assignments = []
   attendees.each_with_index do |name|
-    var2 = "Hello, #{name}! You'll be assigned to room FIXME!"
+    var2 = "Hello, #{name}! You'll be assigned to room #{index}!"
     room_assignments.push(var2)
   end
   room_assignments
